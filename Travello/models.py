7ca697afb3847b3049318ccecc801eaedbe35b91
Keshav Yadav <contact.keshavyadav@gmail.com>
2021-02-05 00:44:36 +0530

    #Models and Migration

--- Travello/models.py
+++ Travello/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+# Model migration command
+# 1-> python manage.py makemigrations
+# 2-> python manage.py sqlmigrate Travello migration_number
+# 3-> python manage.py migrate
 
-class Destination:
-    name: str
-    img: str
-    desc: str
-    price: int
-    offer: bool
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
--- TravelloApp/settings.py
+++ TravelloApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,14 @@ WSGI_APPLICATION = 'TravelloApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# -> pip install psycopg2 | connector b/w postgres and django
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'python/django/travello',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
