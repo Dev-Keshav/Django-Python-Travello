0ff5adc78f2de7f8b7c67e45fa7ce71110bed1a5
Keshav Yadav <contact.keshavyadav@gmail.com>
2021-02-05 00:52:16 +0530

    admin pannel and creating superuser

--- Travello/admin.py
+++ Travello/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Destination
 
 # Register your models here.
+
+# create superuser -> python manage.py createsuperuser
+
+admin.site.register(Destination)
