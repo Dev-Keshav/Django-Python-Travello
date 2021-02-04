1ce14d2b6515bbd75219381d2c1d86927ced7a91
Keshav Yadav <contact.keshavyadav@gmail.com>
2021-02-04 22:12:35 +0530

    treatment for images

--- Travello/views.py
+++ Travello/views.py
@@ -9,6 +9,7 @@ def index(request):
     dest1 = Destination()
     dest1.name = 'New Delhi'
     dest1.desc = 'The capital city of india'
+    dest1.img = 'delhi.jpg'
     dest1.price = 500
 
     return render(request, 'index.html', {'dest1': dest1})
